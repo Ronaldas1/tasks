8af48d725413a31fe4d1fe7bd531c1c793ad4bca
Paulius Zubavičius <paulius@zubavicius.lt>
2021-05-08 11:41:08 +0300

    Consistency check introduced

---
+++ tasks/src/main/java/lt/ronaldas/tasks/cycles/consistency/ConsistencyArr.java
@@ -0,0 +1,10 @@
+package lt.ronaldas.tasks.cycles.consistency;
+
+public class ConsistencyArr {
+
+	public boolean isConsistent(int[] values, int maxStep) {
+		// TODO ar yra nuoseklumas per maxStep dydi
+		return false;
+	}
+
+}
