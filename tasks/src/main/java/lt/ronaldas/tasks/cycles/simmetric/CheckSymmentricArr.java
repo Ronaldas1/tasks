2665665b8dddfa88a24d91d450b42c50575b6dcf
Paulius Zubavičius <paulius@zubavicius.lt>
2021-05-08 11:40:30 +0300

    Symmetric arr impl

---
+++ tasks/src/main/java/lt/ronaldas/tasks/cycles/simmetric/CheckSymmentricArr.java
@@ -0,0 +1,50 @@
+package lt.ronaldas.tasks.cycles.simmetric;
+
+/**
+ * 
+ * Check if arr symmetric or not
+ * 
+ * 
+ *
+ * <pre>
+		i:		 0  1  2  3  4  5  6
+		[]:		[0, 1, 2, 3, 2, 1, 0]
+		 
+		i:		 0  1  2  3  4  5
+		[]:		[a, b, c, c, b, a]
+ * </pre>
+ * 
+ * 
+ * 
+ */
+public class CheckSymmentricArr {
+
+	public boolean isSummetric(String... arr) {
+
+		int center = arr.length / 2;
+		int lastIndex = arr.length - 1;
+
+		for (int i = 0; i < center; i++) {
+			if (!arr[i].equalsIgnoreCase(arr[lastIndex - i])) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new CheckSymmentricArr().isSummetric("1", "2", "4", "4", "2", "1"));
+
+		String polindrome = "Anna";
+
+//		
+//		System.out.println(new CheckSymmentricArr()
+//				.isSummetric(Stream.of(polindrome.toCharArray()).map(Character::toString).toArray(String[]::new)));
+
+		// The same!
+		System.out.println(new CheckSymmentricArr().isSummetric("A", "n", "n", "a"));
+
+	}
+
+}
