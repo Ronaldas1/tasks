aa23f57d704b2d96bfd22bc5940fbe389ad1fc11
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2021-05-14 17:46:32 +0300

    Task ConsistencyArr

--- tasks/src/main/java/lt/ronaldas/tasks/cycles/consistency/ConsistencyArr.java
+++ tasks/src/main/java/lt/ronaldas/tasks/cycles/consistency/ConsistencyArr.java
@@ -4,7 +4,21 @@ public class ConsistencyArr {
 
 	public boolean isConsistent(int[] values, int maxStep) {
 		// TODO ar yra nuoseklumas per maxStep dydi
+		// int i = 0;
+		// int maxStep = values[i++] - values[i];
+		for (int i = 0; i < values.length - 1; i++) {
+			if (values[i++] - values[i] == values[1] - values[0]) {
+				return true;
+			}
+
+		}
 		return false;
 	}
 
+	public static void main(String[] args) {
+		int i = 0;
+		int[] values = { 1, 2, 4, 4, 2, 1 };
+		int maxStep = values[i++] - values[i];
+		System.out.println(new ConsistencyArr().isConsistent(values, 0));
+	}
 }
