bf51a1c501def56813587f756efddd3b637f2dcd
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2021-05-21 21:39:11 +0300

    InvestmentTask

--- tasks/src/main/java/lt/ronaldas/tasks/cycles/investment/InvestmentTask.java
+++ tasks/src/main/java/lt/ronaldas/tasks/cycles/investment/InvestmentTask.java
@@ -12,14 +12,19 @@ public class InvestmentTask {
 	 * @return suma su visais pelnais ir pradine investicija.
 	 */
 	public double makeMoney(double startingMoney, double incomeRate, int periods) {
+		double startingMoney1 = 100;
+		double incomeRate1 = 0.1;
+		double periods1 = (startingMoney1 * incomeRate1)+startingMoney1;
+		//periods = periods - 1;
 
 		return 0.0;
 	}
 
 	public static void main(String[] args) {
-
+		InvestmentTask rezult = new InvestmentTask();
 		// Expecting: 133.1
 		System.out.println(new InvestmentTask().makeMoney(100, 0.1, 3));
+		System.out.println(rezult.makeMoney(100, 0.1, 1));
 
 	}
 }
