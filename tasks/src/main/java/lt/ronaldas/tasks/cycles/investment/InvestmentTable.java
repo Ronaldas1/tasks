a4521e43c1f5b22a716c3398f489bc96ae8eca1c
Paulius Zubavičius <paulius@zubavicius.lt>
2021-05-22 09:54:25 +0300

    Generate table data

--- tasks/src/main/java/lt/ronaldas/tasks/cycles/investment/InvestmentTable.java
+++ tasks/src/main/java/lt/ronaldas/tasks/cycles/investment/InvestmentTable.java
@@ -1,7 +1,16 @@
 package lt.ronaldas.tasks.cycles.investment;
 
-
 // HTML heranrating
 public class InvestmentTable {
+
+	public double[] makeMoney(double startingMoney, double incomeRate, int periods) {
+
+		return null;
+	}
+
+	public String makeHtml(double[] data) {
+
+		return null;
+	}
 
 }
