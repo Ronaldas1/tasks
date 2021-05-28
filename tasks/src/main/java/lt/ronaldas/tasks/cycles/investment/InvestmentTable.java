7f422546b6224ef8bb60b921cc9ecc40e16bbc54
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2021-05-28 22:15:09 +0300

    Investment Table

--- tasks/src/main/java/lt/ronaldas/tasks/cycles/investment/InvestmentTable.java
+++ tasks/src/main/java/lt/ronaldas/tasks/cycles/investment/InvestmentTable.java
@@ -4,13 +4,29 @@ package lt.ronaldas.tasks.cycles.investment;
 public class InvestmentTable {
 
 	public double[] makeMoney(double startingMoney, double incomeRate, int periods) {
+		double[] data = new double[periods];
 
-		return null;
+		for (int i = 0; i < periods; i++) {
+			// data[i] = startingMoney;
+			startingMoney = (startingMoney * incomeRate) + startingMoney;
+			data[i] = startingMoney;
+		}
+
+		return data;
 	}
 
 	public String makeHtml(double[] data) {
+		data.toString();
 
-		return null;
+		return data.toString();
 	}
 
+	public static void main(String[] args) {
+		InvestmentTable invest = new InvestmentTable();
+		double[] rezult = invest.makeMoney(100, 0.1, 3);
+		for (int i = 0; i < rezult.length; i++) {
+			System.out.println(rezult[i]);
+		}
+
+	}
 }
