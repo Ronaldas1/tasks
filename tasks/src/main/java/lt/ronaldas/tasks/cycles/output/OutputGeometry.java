c5d53884f1885e5093d360274ac5ed74fef5e9ed
Paulius Zubavičius <paulius@zubavicius.lt>
2021-07-03 08:55:46 +0300

    New task

---
+++ tasks/src/main/java/lt/ronaldas/tasks/cycles/output/OutputGeometry.java
@@ -0,0 +1,19 @@
+package lt.ronaldas.tasks.cycles.output;
+
+public class OutputGeometry {
+
+	public static void main(String[] args) {
+
+		int[] skaiciai = { 2, 5, 7, 0, 7, 22 };
+
+		soundBars(skaiciai);
+
+	}
+
+	static void soundBars(int[] data) {
+
+		// ???
+
+	}
+
+}
