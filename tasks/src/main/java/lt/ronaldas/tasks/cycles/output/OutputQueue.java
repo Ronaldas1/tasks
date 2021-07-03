c1d7e69f27608eb380c694e0a14fa70430cdbd04
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2021-07-03 08:48:57 +0300

    OutputQueue 2 varj

--- tasks/src/main/java/lt/ronaldas/tasks/cycles/output/OutputQueue.java
+++ tasks/src/main/java/lt/ronaldas/tasks/cycles/output/OutputQueue.java
@@ -17,18 +17,28 @@ public class OutputQueue {
 
 		// 0, 1, 2... 0, -1, -2... => ... 3, 2, 1, 0
 		// FIXME :)
-		for (int i = 0; i < vardai.length; i++) {
+		for (int i = vardai.length - 1; i >= 0; i--) {
 			System.out.println("Vardas    : " + vardai[i]);
+			//break;
 		}
-	}
+	} 
 
 	// Lyginiai
 	private static void outEven(String[] vardai) {
 		// TODO
+		for (int i = 1; i < vardai.length; i+=2) {
+			//if (i % 2 != 0)
+				System.out.println("Even  : " + vardai[i]);
+		}
+
 	}
 
 	// Nelyginiai
 	private static void outOdd(String[] vardai) {
 		// TODO
+		for (int i = 0; i < vardai.length; i+=2) {
+			//if (i % 2 == 0)
+				System.out.println("Odd  :  " + vardai[i]);
+		}
 	}
 }
