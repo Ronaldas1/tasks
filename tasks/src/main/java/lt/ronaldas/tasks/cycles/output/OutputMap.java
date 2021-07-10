e24ecb11f125877b8fd326934530964e792681a4
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2021-07-10 11:28:12 +0300

    Map 1 tasko skanavimas veikia

--- tasks/src/main/java/lt/ronaldas/tasks/cycles/output/OutputMap.java
+++ tasks/src/main/java/lt/ronaldas/tasks/cycles/output/OutputMap.java
@@ -5,53 +5,55 @@ public class OutputMap {
 	public static void main(String[] args) {
 
 		// 3x3x2
-		char[][][] gelmes = {
-				{
-					{'G','G'}, // [0][0]
-					{'G','A'}, // [0][1]
-					{'G','G'}  // [0][2]
-				},{
-					{'A','A'}, // [1][0]
-					{'G','G'}, // [1][1]
-					{'G','G'}  // [1][2]
-				},{
-					{'G','G'}, // [2][0]
-					{'A','G'}, // [2][1]
-					{'A','G'}  // [2][2]
-				}
-		};
-		
+		char[][][] gelmes = { { { 'G', 'G' }, // [0][0]
+				{ 'G', 'A' }, // [0][1]
+				{ 'G', 'G' } // [0][2]
+				}, { { 'A', 'A' }, // [1][0]
+						{ 'G', 'G' }, // [1][1]
+						{ 'G', 'G' } // [1][2]
+				}, { { 'G', 'G' }, // [2][0]
+						{ 'A', 'G' }, // [2][1]
+						{ 'A', 'G' } // [2][2]
+				} };
+
 		char tikslas = 'A';
-		
-		System.out.println( gylioSkenavimas(gelmes[2][1], tikslas));
+
+		System.out.println(gylioSkenavimas(gelmes[2][1], tikslas));
 		System.out.println(gelmes.length);
-		//System.out.println();
-		
+		// System.out.println();
+
 	}
-	
+
 	private static int gylioSkenavimas(char[] gylis, char ieskoma) {
 		// TODO jei gylis atitiks: {'A','G'}
 		// turi grazinti sluoksniu kieki, kiek rado ieskomo elemento
 		// ieskoma: 'A' -> 1
 		// ieskoma: 'G' -> 1
 		// ieskoma: ... -> 0
-		char ieskoma1 = 'A';
-		char ieskoma2 = 'G';
-		for (char i = ieskoma1; i < gylis.length-1; i-- ) {
-			for (char j = ieskoma2; j < gylis.length-1; j-- ) {
-				//if (ieskoma = 'A')
-				//return ieskoma2;
-				//System.out.println("G  =" + ieskoma2);
+		 int kiekSurado = 0;
+//		for (int i = 0; i < gylis.length; i++) {
+//			if (gylis[i] == ieskoma) {
+//				kiekSurado++;
+//			}
+//			//sluoksnis = gylis[i]
+//		}
+		for (char sluoksnis : gylis) {
+			if (sluoksnis == ieskoma) {
+				kiekSurado++;
 			}
-		}
+		} 
 
-		return ieskoma1; 
-		
+		return kiekSurado;
+
 	}
 
 	private static int[][] zemelapioSkenavimas(char[][][] zemelapis) {
 		// Skenuosim per X, Y
 		// Skaiciuosim kiek ir desim reiksmes i int[][]
+		char x = 'A';
+
+		char zemelapis1[][][] = { {}, {}, {} };
+		// if ( x|| "A" )
 
 		return null;
 	}
